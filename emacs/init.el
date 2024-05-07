237fa3ff00e088eb9d5c2b48e606b0750e23ff0f
Kovacs Ervin <kov.ervin97@gmail.com>
2024-05-07 14:11:26 +0300

    sync

--- emacs/init.el
+++ emacs/init.el
@@ -6,13 +6,10 @@
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
 
-;; Don't show the splash screen
-(setq inhibit-startup-message t)
-
-;; Turn off some unneeded UI elements
-(menu-bar-mode -1)
-(tool-bar-mode -1)
-(scroll-bar-mode -1)
+;; Line numbers
+(setq display-line-numbers 'relative)
+(defvar my-linum-format-string "%3d")
+(global-display-line-numbers-mode 1)
 
 ;; Font
 (set-frame-font "Iosevka Nerd Font" )
@@ -21,12 +18,14 @@
 (windmove-default-keybindings)
 
 ;; Color theme
-(package-install 'gruber-darker-theme)
-(package-install 'catppuccin-theme)
+;; (package-install 'gruber-darker-theme)
+;; (package-install 'catppuccin-theme)
+(package-install 'creamsody-theme)
 
 ;; (load-theme 'gruber-darker t)
-(setq catppuccin-flavor 'frappe) 
-(load-theme 'catppuccin :no-confirm)
+;; (setq catppuccin-flavor 'frappe) 
+;; (load-theme 'catppuccin :no-confirm)
+(load-theme 'creamsody :no-confirm)
 
 ;; Recent files
 (recentf-mode 1)
@@ -55,14 +54,17 @@
 (lsp-mode 1)
 
 ;; Rust
-(require 'rust-mode)
-(rust-mode 1)
+;; (require 'rust-mode)
+;; (rust-mode 1)
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("986cfa891116be38a60d1e82d820965249ea44e0d6348634a40ef6827f27bbb0"
+     default))
  '(package-selected-packages nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
