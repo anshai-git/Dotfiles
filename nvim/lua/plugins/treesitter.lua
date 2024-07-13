b6bda6afb5fa7eb4ca70a9e4d73a7b9c1fa592ef
Kovacs Ervin <kov.ervin97@gmail.com>
2024-07-13 18:29:43 +0300

    sync

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,3 +1,11 @@
 return {
-	'nvim-treesitter/nvim-treesitter'
+	'nvim-treesitter/nvim-treesitter',
+  config = function ()
+    require 'nvim-treesitter' .setup {
+      highlight = {
+        enable = true,
+        disable = { "kt", "kotlin" }
+      }
+    }
+  end
 }
